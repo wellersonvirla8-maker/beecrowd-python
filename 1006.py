952906e2987a485494c0e6725b48ca018f0cc410
wellersonvirla8-maker <wellersonvirla8@gmail.com>
2026-09-26 03:04:10 -0300

    Exercicio 1006.py resolvido

---
+++ 1006.py
@@ -0,0 +1,5 @@
+A = float(input("Digite um número: "))
+B = float(input("Digite um número: "))
+C = float(input("Digite outro número: "))
+media = (A * 2 + B * 3 + C * 5)/10
+print(f'MEDIA = {media:.1f}')
